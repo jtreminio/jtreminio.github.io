2abdce6e5681cad803c89a525d426fe511df590f
Juan Treminio <jtreminio@gmail.com>
2018-08-05 20:39:07 -0500

    Use feather font vs icons

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
-    $('div.post h2, div.post h3').each(function () {
+    $('div.post h2, div.post h3, div.post h4').each(function () {
         $(this)
             .append(
-                '<a class="anchor-link" href=#' + $(this).context.id + '>' +
-                '<i data-feather="link"></i></a>'
+                '<a class="anchor-link" href="#' + $(this).context.id + '">' +
+                '<i class="feather icon-link"></i></a>'
             )
             .addClass('anchor-link-container');
     });
-
-    feather.replace();
 });
